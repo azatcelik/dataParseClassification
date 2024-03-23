22645cf21302f662fe3debfc34f93e05cc35726a
Azat Çelik <98810499+azatcelik@users.noreply.github.com>
2024-03-23 12:11:16 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    name="dataparser-cls",  
+    name="dataParser-cls",  
     version="0.1",
     author="azatcelik (Azat Çelik)",
     author_email="azatcellk@hotmail.com",
     description="Data parser for classification",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://https://github.com/azatcelik/dataParseClassification/",
+    url="https://github.com/azatcelik/dataParseClassification/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
